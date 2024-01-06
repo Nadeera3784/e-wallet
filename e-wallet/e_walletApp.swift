7c4192c28c3ffe5b063a10610ee1ca08212992c4
Nadeera Sampath <nadeera3784@gmail.com>
2024-01-07 02:11:03 +0530

    Initial Commit

---
+++ e-wallet/e_walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_walletApp.swift
+//  e-wallet
+//
+//  Created by Nadeera Sampath on 2024-01-07.
+//
+
+import SwiftUI
+
+@main
+struct e_walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
